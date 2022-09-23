9d94746836a26361c5ad2917827a66dea2038db2
miki0221 <miki.shibutani0221@gmail.com>
2022-09-23 18:38:56 +0900

    バリデーション設定、ログイン/ログアウト状態での表示変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,17 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
+  
+  validates :nickname, presence: true
+  validates :encrypted_password, presence: true, length: {minimum: 6}, format: {with: /\A[a-zA-Z0-9]+\z/, message: '英数字6文字以上で入力してください' }
+  validates :last_name_kanji, presence: true, format: {with: /\A[一-龥]+\z/ }
+  validates :first_name_kanji, presence: true, format: {with: /\A[一-龥]+\z/ }
+  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+  validates :birth_id, presence: true, numericality: {other_than: 1}
+
+  has_many :products
+  has_many :buys
+
 end
