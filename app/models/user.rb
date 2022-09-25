bd696aa32ce19bae5952589c1fb8c873d8e1d62b
miki0221 <miki.shibutani0221@gmail.com>
2022-09-25 16:08:25 +0900

    ユーザー管理登録の実装（rubocop実施済）

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,18 +2,17 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :validatable
-  
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+         :recoverable, :rememberable, :validatable
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   validates :nickname, presence: true
   validates :password, format: { with: VALID_PASSWORD_REGEX }
-  validates :last_name_kanji, presence: true, format: {with: /\A[一-龥]+\z/ }
-  validates :first_name_kanji, presence: true, format: {with: /\A[一-龥]+\z/ }
-  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+  validates :last_name_kanji, presence: true, format: { with: /\A[一-龥]+\z/ }
+  validates :first_name_kanji, presence: true, format: { with: /\A[一-龥]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birth_id, presence: true
 
   has_many :products
   has_many :buys
-
 end
