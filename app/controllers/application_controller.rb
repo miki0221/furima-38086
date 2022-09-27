4237d0441fc98130a361f35e75eb0d8292c92926
miki0221 <miki.shibutani0221@gmail.com>
2022-09-28 02:29:10 +0900

    パラメーターの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :encrypted_password, :last_name_kanji, :first_name_kanji,
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name_kanji, :first_name_kanji,
                                                        :last_name_kana, :first_name_kana, :birth_id])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:encrypted_password])
   end
 end
