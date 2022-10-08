792725a1ed248c9b3afba546835c9d300cc46af6
miki0221 <miki.shibutani0221@gmail.com>
2022-10-08 16:21:22 +0900

    バリデーションの修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 class Product < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
-  has_one :buy
+  # 購入機能時に実装するため一時的にコメントアウト
+  # has_one :buy
   has_one_attached :image
 
   validates :image, presence: true, blob: { content_type: :image }
@@ -9,5 +10,5 @@ class Product < ApplicationRecord
   validates :category_id, :condition_id, :postage_id, :area_id, :shipping_day_id, presence: true,
                                                                                   numericality: { other_than: 1, message: "can't be blank" }
   validates :price, presence: true,
-                    numericality: { with: /\A[0-9]+\z/, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
